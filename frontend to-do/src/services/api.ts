9f260bdf1ea6dc7754ef409949a4209c3efe9571
Sadasoumare-21 <sadasoumare018@gmail.com>
2026-07-02 14:26:04 +0000

    reconfiguration des regles du backend

--- frontend to-do/src/services/api.ts
+++ frontend to-do/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 🟢 MODE DE TEST SANS BASE DE DONNÉES / SANS BACKEND
 // Change à true pour tester le Frontend de façon 100% autonome via localStorage.
 // Change à false pour connecter l'application à l'API NestJS réelle et sa base de données MySQL.
-export const USE_MOCK = true;
+export const USE_MOCK = false;
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'https://localhost:3000', // L'adresse de ton Backend NestJS
